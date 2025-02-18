48299e69520431993d52ea835493ae29d3be97d4
Hrithik-Gavankar <hrithikgavankar8582@gmail.com>
2025-02-18 23:15:42 +0530

    cors setup

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"]
+}));
+
+app.use(express.json());ç
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.listen(3000, () => {
-  console.log("App is running on port 3000");
+app.listen(8000, () => {
+  console.log("App is running on port 8000");
 });
